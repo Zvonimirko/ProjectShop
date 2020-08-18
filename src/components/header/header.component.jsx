da49c3b3a0466eca3c5ba347911de0d81297337e
Zvonimir <44640961+Zvonimirko@users.noreply.github.com>
2020-08-18 15:55:17 +0200

    Created project ,added homepage(directory, MenuItem) and added styles

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
