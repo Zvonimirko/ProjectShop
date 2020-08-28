a892c2c159eb31e3e12b68db1f3b443ada41f917
Zvonimir <44640961+Zvonimirko@users.noreply.github.com>
2020-08-28 13:10:45 +0200

    Implemented redux persist storage

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate} from 'redux-persist/integration/react'
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { store, persistor } from './redux/store'
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </Router>
   </Provider>,
